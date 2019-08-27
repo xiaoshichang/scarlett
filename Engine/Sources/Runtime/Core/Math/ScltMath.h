fc9254258ed57c9157f8ae47395d9b4c246a1c8b
xiaoshichang <511680777@qq.com>
2019-08-27 10:26:01 +0800

    1. some basic camera math function and test.

--- Engine/Sources/Runtime/Core/Math/ScltMath.h
+++ Engine/Sources/Runtime/Core/Math/ScltMath.h
@@ -1,14 +1,105 @@
 #include <Eigen3/include/eigen3/Eigen/Dense>
+#include <Eigen3/include/eigen3/Eigen/Geometry>
 
+using namespace Eigen;
 
 namespace Scarlett {
-	typedef Eigen::Vector2f Vector2f;
-	typedef Eigen::Vector3f Vector3f;
-	typedef Eigen::Vector4f Vector4f;
+	const float PI = 3.141592f;
 
-	typedef Eigen::Matrix4f Matrix4f;
-	typedef Eigen::Matrix3f Matrix3f;
+	inline Matrix4f BuildViewLookatRH(const Vector3f& eye, const Vector3f& lookat, const Vector3f& up) {
+		Vector3f xaxis, yaxis, zaxis;
+		float tmp1, tmp2, tmp3;
+		zaxis = eye - lookat;
+		zaxis.normalize();
 
+		xaxis = up.cross(zaxis);
+		xaxis.normalize();
+
+		yaxis = zaxis.cross(xaxis);
+
+		tmp1 = -1 * xaxis.dot(eye);
+		tmp2 = -1 * yaxis.dot(eye);
+		tmp3 = -1 * zaxis.dot(eye);
+
+		Matrix4f tmp;
+		tmp << 
+			xaxis.x(), xaxis.y(), xaxis.z(), tmp1,
+			yaxis.x(), yaxis.y(), yaxis.z(), tmp2,
+			zaxis.x(), zaxis.y(), zaxis.z(), tmp3,
+			0, 0, 0, 1.0f;
+		return tmp;
+	}
+
+	inline Matrix4f BuildViewLookatLH(const Vector3f& eye, const Vector3f& lookat, const Vector3f& up) {
+		Vector3f xaxis, yaxis, zaxis;
+		float tmp1, tmp2, tmp3;
+		zaxis = lookat - eye;
+		zaxis.normalize();
+
+		xaxis = up.cross(zaxis);
+		xaxis.normalize();
+
+		yaxis = zaxis.cross(xaxis);
+
+		tmp1 = -1 * xaxis.dot(eye);
+		tmp2 = -1 * yaxis.dot(eye);
+		tmp3 = -1 * zaxis.dot(eye);
+
+		Matrix4f tmp;
+		tmp <<
+			xaxis.x(), xaxis.y(), xaxis.z(), tmp1,
+			yaxis.x(), yaxis.y(), yaxis.z(), tmp2,
+			zaxis.x(), zaxis.y(), zaxis.z(), tmp3,
+			0, 0, 0, 1.0f;
+		return tmp;
+	}
+
+	inline Matrix4f BuildPerspectiveFovRH(const float fov, const float aspect, const float zn, const float zf) {
+		auto yscale = 1.0f / tanf(fov * 0.5f);
+		auto xscale = yscale / aspect;
+
+		Matrix4f tmp;
+		tmp <<
+			xscale, 0.0f, 0.0f, 0.0f,
+			0.0f, yscale, 0.0f, 0.0f,
+			0.0f, 0.0f, zf / (zn - zf), (zn * zf) / (zn - zf),
+			0.0f, 0.0f, -1.0f, 0.0f;
+		
+		return tmp;
+	}
+
+	inline Matrix4f BuildPerspectiveFovLH(const float fov, const float aspect, const float zn, const float zf) {
+		auto yscale = 1.0f / tanf(fov * 0.5f);
+		auto xscale = yscale / aspect;
+
+		Matrix4f tmp;
+		tmp <<
+			xscale, 0.0f, 0.0f, 0.0f,
+			0.0f, yscale, 0.0f, 0.0f,
+			0.0f, 0.0f, zf / (zf - zn), (-zn * zf) / (zf - zn),
+			0.0f, 0.0f, 1.0f, 0.0f;
+		return tmp;
+	}
+
+	inline Matrix4f BuildOrthoRH(const float width, const float height, const float zn, const float zf) {
+		Matrix4f tmp;
+		tmp <<
+			2 / width, 0.0f, 0.0f, 0.0f,
+			0.0f, 2 / height, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f / (zn - zf), zn / (zn - zf),
+			0.0f, 0.0f, 0.0f, 1.0f;
+		return tmp;
+	}
+
+	inline Matrix4f BuildOrthoLH(const float width, const float height, const float zn, const float zf) {
+		Matrix4f tmp;
+		tmp <<
+			2 / width, 0.0f, 0.0f, 0.0f,
+			0.0f, 2 / height, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f / (zf - zn), -zn / (zf - zn),
+			0.0f, 0.0f, 0.0f, 1.0f;
+		return tmp;
+	}
 
 }
 
--- Tests/Test_Math.cpp
+++ Tests/Test_Math.cpp
@@ -6,13 +6,38 @@ using namespace Scarlett;
 int main() {
 	Matrix3f m;
 	m << 1, 2, 3, 4, 5, 6, 7, 8, 9;
-	std::cout << m << std::endl;
+	std::cout << m << std::endl << std::endl;
 
 	Matrix3f t = m.transpose();
-	std::cout << t << std::endl;
+	std::cout << t << std::endl << std::endl;
 
 	Matrix4f r = Matrix4f::Random();
-	std::cout << r << std::endl;
+	std::cout << r << std::endl << std::endl;
+
+	auto translation = Affine3f(Translation3f(1.0f, 2.0f, 3.0f));
+	std::cout << translation.matrix() << std::endl << std::endl;
+
+	auto scale = Affine3f(Scaling(2.0f, 2.0f, 2.0f));
+	std::cout << scale.matrix() << std::endl << std::endl;
+
+	Vector3f eye = { 1.0f, 1.0f, 1.0f };
+	Vector3f lookat = { 0.0f, 0.0f, 0.0f };
+	Vector3f up = { 0.0f, 1.0f, 0.0f };
+
+	auto view1 = BuildViewLookatLH(eye, lookat, up);
+	std::cout << view1 << std::endl << std::endl;
+	auto view2 = BuildViewLookatRH(eye, lookat, up);
+	std::cout << view2 << std::endl << std::endl;
+
+	auto p1 = BuildOrthoRH(1024.0f, 768.0f, 1.0f, 1000.0f);
+	std::cout << p1 << std::endl << std::endl;
+	auto p2 = BuildOrthoLH(1024.0f, 768.0f, 1.0f, 1000.0f);
+	std::cout << p2 << std::endl << std::endl;
+
+	auto p3 = BuildPerspectiveFovRH(PI * 0.5, 1024.0f / 768.0f, 1.0f, 1000.0f);
+	std::cout << p3 << std::endl << std::endl;
+	auto p4 = BuildPerspectiveFovLH(PI * 0.5, 1024.0f / 768.0f, 1.0f, 1000.0f);
+	std::cout << p4 << std::endl << std::endl;
 
 	return 0;
 }
